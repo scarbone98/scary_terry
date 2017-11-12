16454a7012f0624b72630a863e335fd5369a0a44
scarbone98 <scarbon@purdue.edu>
2017-11-11 21:53:41 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+/**
+ * Created by scarbone on 11/11/17.
+ */
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.json());
+app.use('/',express.static('static'));
+
+app.get('/', function (req, res) {
+
+});
+
+let port = process.env.PORT || 8000;
+app.listen(port);
